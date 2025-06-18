ba5ae9a0dcba84be0dc4ea3f4d0b4969ad589dd1
imanishita <imanishita17@gmail.com>
2025-06-18 22:57:45 +0530

    twoservicesworkingfine

--- userservice/src/main/java/com/fitness/userservice/UserserviceApplication.java
+++ userservice/src/main/java/com/fitness/userservice/UserserviceApplication.java
@@ -2,8 +2,14 @@ package com.fitness.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    MongoAutoConfiguration.class,
+    MongoDataAutoConfiguration.class
+})
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
